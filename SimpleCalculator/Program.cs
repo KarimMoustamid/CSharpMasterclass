e72a333116eff033fe960e31a38949aa7a7d0b6d
karimmoustamid <moustamid.teck@gmail.com>
2024-01-15 17:53:51 -0600

    Remove 'TodoList' project from solution
    The 'TodoList' project reference and associated configurations have been deleted from the CsharpMasterClass solution file. This was done to clean up the solution and keep it focused on relevant projects.

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,54 @@
+﻿
+SimpleCalculator();
+Console.WriteLine("\n\nPress any key to close");
+Console.ReadKey();
+
+
+
+#region Methods
+
+static void SimpleCalculator()
+{
+    Console.WriteLine("\n\nHello!");
+
+    var firstNumber = GetNumberFromUser("Input the first number :");
+    var secoundNumber = GetNumberFromUser("Input the second number :");
+
+    Console.WriteLine("\nWhat do you want to do?");
+    Console.WriteLine("[A]dd numbers");
+    Console.WriteLine("[S]ubtract numbers");
+    Console.WriteLine("[M]ultiply numbers");
+    
+    var userChoiceInput = Console.ReadLine();
+    switch (userChoiceInput.ToUpper())
+    {
+        case "A":
+            var addition = firstNumber + secoundNumber;
+            Console.WriteLine($"\nthe Addition of {firstNumber} + {secoundNumber} is {addition}");
+            break;
+
+        case "S":
+            var substitution = firstNumber - secoundNumber;
+            Console.WriteLine($"\nthe substitution of {firstNumber} - {secoundNumber} is {substitution}");
+            break;
+
+        case "M":
+            var multiplication = firstNumber * secoundNumber;
+            Console.WriteLine($"\nthe substitution of {firstNumber} * {secoundNumber} is {multiplication}");
+            break;
+
+        default:
+            Console.WriteLine("Invalid Choice!");
+            break;
+    }
+    
+}
+
+static float GetNumberFromUser(string prompt)
+{
+    Console.WriteLine(prompt);
+    var numberInput = Console.ReadLine();
+    return float.Parse(numberInput);
+}
+
+#endregion
